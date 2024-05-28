7c21ac2d21f3abd13c8eff9aaed62e77b849e70f
Deivid Mateo Cañan Pretel <149835310+DeividMateo@users.noreply.github.com>
2024-05-28 16:45:13 -0500

    docu

--- src/main/java/co/edu/uniquindio/poo/Propietario.java
+++ src/main/java/co/edu/uniquindio/poo/Propietario.java
@@ -1,3 +1,10 @@
+/**
+ * La clase Propietario representa a un propietario de un objeto o propiedad.
+ * Cada propietario tiene un nombre, apellido, edad, número de celular y correo electrónico.
+ * 
+ * @author Deivid Mateo Cañan, Kevin Lopez, Nicolas Loaiza
+ * @version 1.0
+ */
 package co.edu.uniquindio.poo;
 
 public class Propietario {
@@ -7,6 +14,16 @@ public class Propietario {
     private final int edad;
     private final String celular;
     private final String correo;
+
+     /**
+     * Crea un nuevo Propietario con los detalles proporcionados.
+     *
+     * @param nombre   El nombre del propietario. No puede ser null ni estar en blanco.
+     * @param apellido El apellido del propietario. No puede ser null ni estar en blanco.
+     * @param edad     La edad del propietario. Debe ser mayor que 0.
+     * @param celular  El número de celular del propietario. No puede ser null ni estar en blanco.
+     * @param correo   El correo electrónico del propietario. Debe contener el símbolo @.
+     */
     
     
     
@@ -22,26 +39,48 @@ public class Propietario {
         this.celular = celular;
         this.correo = correo;
     }
-
+  /**
+     * Devuelve el nombre del propietario.
+     *
+     * @return El nombre del propietario.
+     */
     public String getNombre() {
         return nombre;
     }
+    
+    /**
+     * Devuelve el apellido del propietario.
+     *
+     * @return El apellido del propietario.
+     */
 
 
     public String getApellido() {
         return apellido;
     }
-
+  /**
+     * Devuelve la edad del propietario.
+     *
+     * @return La edad del propietario.
+     */
 
     public int getEdad() {
         return edad;
     }
-
+/**
+     * Devuelve el celular del propietario.
+     *
+     * @return el celular del propietario.
+     */
 
     public String getCelular() {
         return celular;
     }
-
+/**
+     * Devuelve el correo del propietario.
+     *
+     * @return el correp del propietario.
+     */
 
     public String getCorreo() {
         return correo;
@@ -54,3 +93,6 @@ public class Propietario {
 
     
 }
+
+
+
