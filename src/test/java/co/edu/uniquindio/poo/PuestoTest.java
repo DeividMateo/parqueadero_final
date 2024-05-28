a584b3409e89f3befc37e094ff75cc6ecc36f5c5
Nicolas Betancourt <nicolas.loaizab@uqvirtual.edu.co>
2024-05-28 12:10:45 -0500

    Test clase puestos

--- src/test/java/co/edu/uniquindio/poo/PuestoTest.java
+++ src/test/java/co/edu/uniquindio/poo/PuestoTest.java
@@ -1,5 +1,40 @@
 package co.edu.uniquindio.poo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
 public class PuestoTest {
+    private static final Logger LOG = Logger.getLogger(PuestoTest.class.getName());
+
+    @Test
+    public void datosCompletos(){
+
+        LOG.info("Iniciando test de datosCompletos");
+
+        var puesto = new Puesto(1, 1, Estado.LIBRE);
+
+        assertEquals(1, puesto.getPosicionI());
+        assertEquals(1, puesto.getPosicionJ());
+        assertEquals(Estado.LIBRE, puesto.getEstado());
+        assertEquals(null, puesto.getVehiculo());
+
+        LOG.info("Finalizando test de datosCompletos");
+
+    }
+
+    @Test 
+    public void datosNegativosYnulos(){
+
+        LOG.info("Iniciando test de datosNegativosYnulos");
+
+        assertThrows(Throwable.class, () -> new Puesto(-1, -1, null));
+        
+        LOG.info("Finalizando test de datosNegativosYnulos");
+        
+    }
     
 }
